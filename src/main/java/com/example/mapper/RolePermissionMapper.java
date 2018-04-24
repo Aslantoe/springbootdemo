69c59384d03a44bed17a5f6e4b2d8bac001364a8
Aslan <jackbu1994@gmail.com>
2018-04-24 20:48:23 +0800

    添角色_权限mapper

---
+++ src/main/java/com/example/mapper/RolePermissionMapper.java
@@ -0,0 +1,12 @@
+package com.example.mapper;
+
+import com.example.entity.RolePermission;
+import com.example.util.MyMapper;
+
+/**
+ * @author jack
+ * @date 2018/4/24 20:47
+ */
+public interface RolePermissionMapper extends MyMapper<RolePermission> {
+
+}
