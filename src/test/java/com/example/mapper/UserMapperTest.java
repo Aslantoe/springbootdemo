4c483d19d49edd263a3d978ad4b8285ebb3276ce
Aslan <jackbu1994@gmail.com>
2018-04-25 14:46:19 +0800

    转移测试方法到impl中并删除测试方法

--- src/test/java/com/example/mapper/UserMapperTest.java
+++ src/test/java/com/example/mapper/UserMapperTest.java
@@ -1,14 +1,9 @@
 package com.example.mapper;
 
-import com.example.entity.UserInfo;
-import com.example.service.UserInfoService;
-import com.example.service.impl.UserInfoServiceImpl;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import tk.mybatis.mapper.entity.Example;
+
 
 /**
  * @author jack
@@ -18,20 +13,6 @@ import tk.mybatis.mapper.entity.Example;
 @SpringBootTest
 public class UserMapperTest {
 
-    @Autowired
-    private UserInfoServiceImpl service;
 
-    @Test
-    public void getUserInfoByName(){
-
-       UserInfo userInfo = service.getUserInfoByName("admin");
-       System.out.println(userInfo.toString());
-
-    }
-    @Test
-    public void getUserInfoByEmail(){
-       UserInfo userInfo = service.getUserInfoByEmail("jackbu1994@gmail.com");
-      System.out.println(userInfo.toString());
-    }
 
 }
