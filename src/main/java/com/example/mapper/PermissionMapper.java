010957116fdd39690c51bb03bc01b554dcf867b5
Aslan <jackbu1994@gmail.com>
2018-04-25 19:12:50 +0800

    添加权限mapper

--- src/main/java/com/example/mapper/PermissionMapper.java
+++ src/main/java/com/example/mapper/PermissionMapper.java
@@ -2,6 +2,10 @@ package com.example.mapper;
 
 import com.example.entity.Permission;
 import com.example.util.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -11,9 +15,44 @@ import java.util.Map;
  * @date 2018/4/24 0024 20:37
  */
 public interface PermissionMapper extends MyMapper<Permission> {
-    List<Permission> queryAll();
-
+    @Select("<script>" +
+            "SELECT * " +
+            "FROM permission p LEFT JOIN role_permission rp " +
+            "ON p.perms_id = rp.perms_id " +
+            "LEFT JOIN user_role ur " +
+            "ON rp.role_id = ur.role_id " +
+            "<where>"+
+            "<if test=\"uid != null \">ur.user_id = #{uid}</if> " +
+            "<if test=\"type != null \"> AND p.perms_res_type = #{type}</if> " +
+            "GROUP BY  p.perms_id"+
+            "</where>"+
+            "</script>")
+    @Results({
+            @Result(property = "permsId",column = "perms_id"),
+            @Result(property = "permsDesc",column = "perms_desc"),
+            @Result(property = "permsResType",column = "perms_res_type"),
+            @Result(property = "permsResUrl",column = "perms_res_url"),
+            @Result(property = "permsParentId",column = "perms_parent_id"),
+            @Result(property = "permsParentIds",column = "perms_parent_ids"),
+            @Result(property = "permsAvailable",column = "perms_available")
+    })
     List<Permission> loadUserPermissions(Map<String,Object> map);
 
-    List<Permission> queryPermissionsListWithSelected(Integer pid);
+
+    @Select("SELECT *  " +
+            "FROM permission p  " +
+            "WHERE p.perms_id IN " +
+            "(SELECT rp.perms_id " +
+            " FROM role_permission rp " +
+            " WHERE rp.role_id = #{rid} )")
+    @Results({
+            @Result(property = "permsId",column = "perms_id"),
+            @Result(property = "permsDesc",column = "perms_desc"),
+            @Result(property = "permsResType",column = "perms_res_type"),
+            @Result(property = "permsResUrl",column = "perms_res_url"),
+            @Result(property = "permsParentId",column = "perms_parent_id"),
+            @Result(property = "permsParentIds",column = "perms_parent_ids"),
+            @Result(property = "permsAvailable",column = "perms_available")
+    })
+    List<Permission> queryPermissionsListWithSelected(@Param("rid") Integer rid);
 }
