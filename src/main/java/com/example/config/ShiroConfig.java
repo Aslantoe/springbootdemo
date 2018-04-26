a96cb14a0b8feb39d4ec9c1af14b8f9bbdce9d1a
Aslan <jackbu1994@gmail.com>
2018-04-26 15:07:34 +0800

    添加ShiroConfig

---
+++ src/main/java/com/example/config/ShiroConfig.java
@@ -0,0 +1,191 @@
+package com.example.config;
+
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+import com.example.entity.Permission;
+import com.example.service.PermissionService;
+import com.example.shiro.MyShiroRealm;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.crazycake.shiro.RedisSessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tk.mybatis.mapper.util.StringUtil;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jack
+ * @date 2018/4/25 0025 21:08
+ */
+@Configuration
+public class ShiroConfig {
+    @Autowired
+    private PermissionService permissionService;
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public static LifecycleBeanPostProcessor getLifecycleBeanPostProcessor(){
+        return new LifecycleBeanPostProcessor();
+    }
+
+    /**
+     * shiroDialect,为了在thymeleaf里使用shiro的标签的bean
+     */
+    public ShiroDialect shiroDialect(){
+        return new ShiroDialect();
+    }
+
+    /**
+     * ShiroFilterFactoryBean处理拦截资源问题。
+     * 注意:单独一个ShiroFilterFactoryBean配置是报错的，
+     * 因为在初始化ShiroFilterFactoryBean的时候需要注入:SecurityManager
+     *
+     * FilterChain定义说明
+     * 1.一个URL可以配置多个Filter，使用逗号分隔
+     * 2.当设置多个过滤器时，全部验证通过，才是为通过
+     * 3.部分过滤器可以指定参数，如perms，roles
+     */
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager){
+        System.out.println("------->ShiroFilterFactoryBean.shiroFilter");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //必须设置SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl("/login_admin");
+        //登录后要跳转的链接
+        shiroFilterFactoryBean.setSuccessUrl("");
+        //未授权界面
+        shiroFilterFactoryBean.setUnauthorizedUrl("");
+        //拦截器
+        Map<String,String> filterChainDefinitionMap = new HashMap<>();
+        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/css/**","anon");
+        filterChainDefinitionMap.put("/js/**","anon");
+        filterChainDefinitionMap.put("/img/**","anon");
+        filterChainDefinitionMap.put("/font-awesome/**","anon");
+        //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
+        //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        //自定义加载权限关系
+        List<Permission> permissionList = permissionService.queryAll();
+        for (Permission permission : permissionList) {
+            if(StringUtil.isNotEmpty(permission.getPermsResUrl())){
+                String perms = "perms[" + permission.getPermsResUrl() + "]";
+                filterChainDefinitionMap.put(permission.getPermsResUrl(), perms);
+            }
+        }
+        filterChainDefinitionMap.put("/**","authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
+        //设置realm
+        securityManager.setRealm(myShiroRealm());
+        //自定义缓存实现 使用redis
+        securityManager.setCacheManager(redisCacheManager());
+        //自定义session管理 使用redis
+        securityManager.setSessionManager(sessionManager());
+        return securityManager;
+    }
+
+    @Bean
+    public MyShiroRealm myShiroRealm(){
+        MyShiroRealm myShiroRealm = new MyShiroRealm();
+        myShiroRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return myShiroRealm;
+    }
+
+  /**
+     * 凭证匹配器
+     * （由于我们的密码校验交给Shiro的SimpleAuthenticationInfo进行处理了
+     *  所以需要修改下doGetAuthenticationInfo中的代码
+     * ）
+     */
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher(){
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        //散列算法:这里使用MD5算法;
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");
+        //散列的次数，比如散列两次，相当于 md5(md5(""));
+        hashedCredentialsMatcher.setHashIterations(2);
+        return hashedCredentialsMatcher;
+    }
+
+    /**
+     *  开启shiro aop注解支持.
+     *  使用代理方式;所以需要开启代码支持;
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+    /**
+     * 配置shiro redisManager
+     * 使用shiro-redis开源插件
+     */
+    @Bean
+    public RedisManager redisManager(){
+        RedisManager redisManager = new RedisManager();
+        redisManager.setHost(host);
+        redisManager.setPort(port);
+        //缓存过期时间
+        redisManager.setExpire(1800);
+        return redisManager;
+    }
+    /**
+     * cacheManager 缓存 redis实现
+     * 使用shiro-redis插件
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(){
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+        redisCacheManager.setRedisManager(redisManager());
+        return redisCacheManager;
+    }
+    /**
+     * RedisSessionDAO shiro sessionDao层的实现 通过redis
+     * 使用shiro-redis开源插件
+     */
+    @Bean
+    public RedisSessionDAO redisSessionDAO(){
+        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+        redisSessionDAO.setRedisManager(redisManager());
+        return redisSessionDAO;
+    }
+    /**
+     * shiro session的管理
+     */
+    @Bean
+    public DefaultWebSessionManager sessionManager(){
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        sessionManager.setSessionDAO(redisSessionDAO());
+        return sessionManager;
+    }
+
+}
