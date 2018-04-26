1cbc65f40e83989fc2e56b51681b0f4d6878cc06
Aslan <jackbu1994@gmail.com>
2018-04-26 14:36:16 +0800

    删除无用导入

--- src/main/java/com/example/shiro/ShiroService.java
+++ src/main/java/com/example/shiro/ShiroService.java
@@ -2,25 +2,17 @@ package com.example.shiro;
 
 import com.example.entity.Permission;
 import com.example.service.PermissionService;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.mgt.DefaultFilterChainManager;
 import org.apache.shiro.web.filter.mgt.PathMatchingFilterChainResolver;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import tk.mybatis.mapper.util.StringUtil;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
+
 
 /**
  * @author jack
