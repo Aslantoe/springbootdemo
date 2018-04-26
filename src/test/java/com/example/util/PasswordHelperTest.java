01fba0a12a3b4e9b24f4e501650134ba397f324c
Aslan <jackbu1994@gmail.com>
2018-04-26 15:44:01 +0800

    加密工具测试类

---
+++ src/test/java/com/example/util/PasswordHelperTest.java
@@ -0,0 +1,23 @@
+package com.example.util;
+
+import com.example.entity.UserInfo;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jack
+ * @date 2018/4/26 0026 15:40
+ */
+public class PasswordHelperTest {
+    @Test
+    public void encryptPassword() throws Exception {
+        PasswordHelper helper = new PasswordHelper();
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserEmail("jackbu@gmail.com");
+        userInfo.setUserPassword("admin");
+        helper.encryptPassword(userInfo);
+        System.out.println(userInfo.toString());
+    }
+
+}
