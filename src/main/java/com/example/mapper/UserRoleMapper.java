f3378c84b3a909483b3f6c57dc840da71015a555
Aslan <jackbu1994@gmail.com>
2018-04-27 17:11:32 +0800

    通过角色id查询用户id

--- src/main/java/com/example/mapper/UserRoleMapper.java
+++ src/main/java/com/example/mapper/UserRoleMapper.java
@@ -2,6 +2,11 @@ package com.example.mapper;
 
 import com.example.entity.UserRole;
 import com.example.util.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
 import java.util.List;
 
 /**
@@ -14,5 +19,10 @@ public interface UserRoleMapper extends MyMapper<UserRole> {
      * @param roleId 角色ID
      * @return 用户Id集合
      */
-    List<Integer> findUserIdByRoleId(Integer roleId);
+    @Select("SELECT * FROM user_role ur WHERE ur.role_id = #{roleId}")
+    @Results({
+            @Result(property = "roleId",column = "role_id"),
+            @Result(property = "userId",column = "user_id")
+    })
+    List<Integer> findUserIdByRoleId(@Param("roleId") Integer roleId);
 }
