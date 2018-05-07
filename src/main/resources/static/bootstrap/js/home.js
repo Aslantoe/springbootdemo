aed2ad5eba59885b0355703f8dcef12cfcbf207e
Aslan <jackbu1994@gmail.com>
2018-05-07 17:20:28 +0800

    ajax——点击轮播图显示详情

--- src/main/resources/static/bootstrap/js/home.js
+++ src/main/resources/static/bootstrap/js/home.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
    });
 });
 
+
 /*点击导航条检索*/
 $(document).ready(function(){
     $(".dropdown-menu li").click(function() {
+
         var param = $(this).children("a").text();
         var str = $(this).parent().parent().children(".dropdown-toggle").text();
         var temp = '';
@@ -29,8 +31,8 @@ $(document).ready(function(){
                         $.each(data,function (i,item) {
                             str += ( "            <div class=\"col-md-3\">\n" +
                                 "                <div class=\"thumbnail\">\n" +
-                                "                    <a id=\"img\">\n" +
-                                "                      <a href='####'> <img src=\" "+item.filmGallery+" \" alt=\" "+item.filmCnName+" \" style=\"height: 345px;width: 100%\">\n </a>" +
+                                "                    <a href='#'>\n" +
+                                "                     <img onclick='getFilmInfo()' id=\"galleryNav\" src=\" "+item.filmGallery+" \" alt=\" "+item.filmCnName+" \" style=\"height: 345px;width: 100%\">\n" +
                                 "                    </a>\n" +
                                 "                    <div class=\"caption\">\n" +
                                 "                        <h3>"+item.filmCnName+"</h3>\n" +
@@ -44,12 +46,20 @@ $(document).ready(function(){
                         /*隐藏轮播图和影视列表*/
                         $("#carousel").attr("style","visibility:hidden");
                         $("#gallery").attr("style","visibility:hidden");
+                        $("#pageContainer").attr("style","visibility:hidden");
                 }
             }
         });
 
     });
+
 });
+
+function getFilmInfo () {
+    var filmName = $("#galleryNav").attr("alt");
+    console.log(filmName);
+    alert(filmName);
+}
 
 /*输入搜索*/
 function getVagueFilm() {
@@ -68,8 +78,8 @@ function getVagueFilm() {
                 $.each(data,function (i,item) {
                     str += ( "            <div class=\"col-md-3\">\n" +
                         "                <div class=\"thumbnail\">\n" +
-                        "                    <a id=\"img\">\n" +
-                        "                       <img src=\" "+item.filmGallery+" \" alt=\" "+item.filmCnName+" \" style=\"height: 345px;width: 100%\">\n" +
+                        "                    <a href='#' id=\"img\">\n" +
+                        "                       <img class=\"gallerySearch\" src=\" "+item.filmGallery+" \" alt=\" "+item.filmCnName+" \" style=\"height: 345px;width: 100%\">\n" +
                         "                    </a>\n" +
                         "                    <div class=\"caption\">\n" +
                         "                        <h3>"+item.filmCnName+"</h3>\n" +
@@ -84,6 +94,7 @@ function getVagueFilm() {
                 /*隐藏轮播图和影视列表*/
                 $("#carousel").attr("style","visibility:hidden");
                 $("#gallery").attr("style","visibility:hidden");
+                $("#pageContainer").attr("style","visibility:hidden");
 
             }else{
                 alert("没有找到相关影片！");
@@ -91,7 +102,64 @@ function getVagueFilm() {
         },
 
     });
+
+
 }
+/*点击轮播图*/
+$(document).ready(function () {
+    $(".poster").click(function () {
+        var filmName = $(this).attr("alt");
+        $.ajax({
+            async:false,
+            type: "POST",
+            url: "getVagueFilms",
+            data:{name:filmName},
+            dataType:"json",
+            success:function(data){
+                if(!jQuery.isEmptyObject(data)){
+                    /*显示DIV*/
+                    $("#filmInfo").attr("style","visibility:visible");
+                    var str = "";
+                    var filmCnName = "";
+                    var introduction = "";
+                    $.each(data,function (i,item) {
+                        console.log(item);
+                        str += ("  <div class=\"col-md-6 col-md-offset-1 fl-img\">\n" +
+                            "            <a class=\"imglink\" href=\"#\" target=\"_blank\"><img src=\""+item.filmGallery+"\" /></a>\n" +
+                            "        </div>\n" +
+                            "        <div class=\"col-md-6 fl-info\">\n" +
+                            "            <ul>\n" +
+                            "                <li class=\"ib\"><span>中文名：</span><strong>"+item.filmCnName+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>英文名：</span><strong>"+item.filmEnName+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>地区：</span><strong>"+item.filmCountry+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>片长：</span><strong>"+item.filmMins+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>语  言：</span><strong>"+item.filmLanguage+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>上映日期：</span><strong>"+item.releaseDate.substring(0,10)+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>豆瓣评分：</span><strong>"+item.filmScore+"</strong></li>\n" +
+                            "                <li class=\"ib\"><span>类型：</span><strong>"+item.filmGenre+"</strong></li>\n" +
+                            "                <li id=\"director\"><span>导演：</span>"+item.filmDirector+"</li>\n" +
+                            "                <li id=\"actor\"><span>主演：</span>"+item.filmActor+"</li>\n" +
+                            "                <a id=\"favorite\">添加到我的收藏</a>\n" +
+                            "            </ul>\n" +
+                            "        </div>");
+                        filmCnName = item.filmCnName;
+                        introduction = item.filmIntroduction;
+                    });
+                    $(".brieftitle").html(filmCnName+"的剧情简介.....");
+                    $(".con span").html(introduction.substring(0,200));
+                    $("#filmInfo-detail").html(str);
+                    /*隐藏轮播图和影视列表*/
+                    $("#carousel").attr("style","display:none");
+                    $("#gallery").attr("style","display:none");
+                    $("#pageContainer").attr("style","display:none");
+
+                }
+            },
+
+        });
+
+    });
+});
 
 
 
