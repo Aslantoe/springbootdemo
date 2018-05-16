646f49a22ce22f87e78600d201036995276ac1ae
Aslan <jackbu1994@gmail.com>
2018-05-17 00:42:15 +0800

    测试查找所有/通过用户查找角色

--- src/test/java/com/example/mapper/UserMapperTest.java
+++ src/test/java/com/example/mapper/UserMapperTest.java
@@ -1,8 +1,15 @@
 package com.example.mapper;
 
+import com.example.manager.entity.User;
+import com.example.manager.entity.UserWithRole;
+import com.example.manager.mapper.UserMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 
 /**
@@ -13,6 +20,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserMapperTest {
 
+    @Autowired
+    private UserMapper mapper;
 
+    @Test
+    public void findUserWithRole(){
+         List<UserWithRole> list = mapper.findUserWithRole(23L);
+        for (UserWithRole user : list) {
+            System.out.println(user.toString());
+        }
+    }
+
+
+    @Test
+    public void findAllUser(){
+        List<User> list = mapper.selectAll();
+        for (User user : list) {
+            System.out.println(user.toString());
+        }
+    }
 
 }
