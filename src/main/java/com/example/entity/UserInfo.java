7a2504e147c00dc5a27991085240d2b81e6b4557
Aslan <jackbu1994@gmail.com>
2018-05-16 22:59:27 +0800

    修改实体类与数据库新表相对应

--- src/main/java/com/example/entity/UserInfo.java
+++ src/main/java/com/example/entity/UserInfo.java
@@ -3,150 +3,263 @@ package com.example.entity;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author jack
- * @date 2018/4/20 0020 14:52
+ * @date 2018/5/16  22:53
  */
-@Table(name = "userinfo")
+@Table(name = "t_user")
 public class UserInfo implements Serializable{
+    private static final long serialVersionUID = 5174769567803601093L;
+
+    /**
+     * 账户有效
+     */
+    public static final String STATUS_VALID = "1";
+    /**
+     * 账户锁定
+     */
+    public static final String STATUS_LOCK = "0";
+
+    public static final String SEX_UNKNOW = "2";
+
+    public static final String DEFAULT_THEME = "green";
+
+    public static final String DEFAULT_AVATAR = "default.jpg";
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Integer userId;
-    @Column(name = "user_nickname")
-    private String userNickName;
-    @Column(name = "user_name")
-    private String userName;
-    @Column(name = "user_password")
-    private String userPassword;
-    @Column(name = "user_salt")
-    private String userSalt;
-    @Column(name = "user_gender")
-    private String userGender;
-    @Column(name = "user_birthday")
-    private Date userBirthday;
-    @Column(name = "user_city")
-    private String userCity;
-    @Column(name = "user_email")
-    private String userEmail;
-    @Column(name = "user_avatar")
-    private String userAvatar;
-    @Column(name = "user_state")
-    private Integer userState;
+    @GeneratedValue(generator = "JDBC")
+    @Column(name = "USER_ID")
+    private Long userId;
 
+    @Column(name = "USERNAME")
+    private String username;
 
-    public Integer getUserId() {
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "DEPT_ID")
+    private Long deptId;
+
+    @Column(name = "EMAIL")
+
+    private String email;
+
+    @Column(name = "MOBILE")
+    private String mobile;
+
+    @Column(name = "STATUS")
+    private String status = STATUS_VALID;
+
+    @Column(name = "CRATE_TIME")
+    private Date crateTime;
+
+    @Column(name = "MODIFY_TIME")
+    private Date modifyTime;
+
+    @Column(name = "LAST_LOGIN_TIME")
+    private Date lastLoginTime;
+
+    @Column(name = "SSEX")
+    private String ssex;
+
+    @Column(name = "THEME")
+    private String theme;
+
+    @Column(name = "AVATAR")
+    private String avatar;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Transient
+    private String roleName;
+
+    /**
+     * @return USER_ID
+     */
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(Integer userId) {
+    /**
+     * @param userId
+     */
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
 
-    public String getUserNickName() {
-        return userNickName;
+    /**
+     * @return USERNAME
+     */
+    public String getUsername() {
+        return username;
     }
-
-    public void setUserNickName(String userNickName) {
-        this.userNickName = userNickName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public String getUserSalt() {
-        return userSalt;
-    }
-
-    public void setUserSalt(String userSalt) {
-        this.userSalt = userSalt;
-    }
-
-    public String getUserGender() {
-        return userGender;
-    }
-
-    public void setUserGender(String userGender) {
-        this.userGender = userGender;
-    }
-
-    public Date getUserBirthday() {
-        return userBirthday;
-    }
-
-    public void setUserBirthday(Date userBirthday) {
-        this.userBirthday = userBirthday;
-    }
-
-    public String getUserCity() {
-        return userCity;
-    }
-
-    public void setUserCity(String userCity) {
-        this.userCity = userCity;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getUserAvatar() {
-        return userAvatar;
-    }
-
-    public void setUserAvatar(String userAvatar) {
-        this.userAvatar = userAvatar;
-    }
-
-    public Integer getUserState() {
-        return userState;
-    }
-
-    public void setUserState(Integer userState) {
-        this.userState = userState;
-    }
-
 
     /**
-     * 密码盐
+     * @param username
      */
-    public String getCredentialsSalt(){
-        return this.userEmail + this.userSalt;
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
     }
 
-    @Override
-    public String toString() {
-        return "UserInfo{" +
-                "userId=" + userId +
-                ", userNickName='" + userNickName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", userPassword='" + userPassword + '\'' +
-                ", userSalt='" + userSalt + '\'' +
-                ", userGender='" + userGender + '\'' +
-                ", userBirthday=" + userBirthday +
-                ", userCity='" + userCity + '\'' +
-                ", userEmail='" + userEmail + '\'' +
-                ", userAvatar='" + userAvatar + '\'' +
-                ", userState=" + userState +
-                '}';
+    /**
+     * @return PASSWORD
+     */
+    public String getPassword() {
+        return password;
     }
+
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    /**
+     * @return DEPT_ID
+     */
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    /**
+     * @param deptId
+     */
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    /**
+     * @return EMAIL
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    /**
+     * @return MOBILE
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * @param mobile
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    /**
+     * @return STATUS
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status
+     */
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    /**
+     * @return CRATE_TIME
+     */
+    public Date getCrateTime() {
+        return crateTime;
+    }
+
+    /**
+     * @param crateTime
+     */
+    public void setCrateTime(Date crateTime) {
+        this.crateTime = crateTime;
+    }
+
+    /**
+     * @return MODIFY_TIME
+     */
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    /**
+     * @param modifyTime
+     */
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    /**
+     * @return LAST_LOGIN_TIME
+     */
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    /**
+     * @param lastLoginTime
+     */
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    /**
+     * @return SSEX
+     */
+    public String getSsex() {
+        return ssex;
+    }
+
+    /**
+     * @param ssex
+     */
+    public void setSsex(String ssex) {
+        this.ssex = ssex == null ? null : ssex.trim();
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
 }
