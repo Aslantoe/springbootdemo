b00685c2383e67e744a802b08dc8af7524cbc49f
Aslan <jackbu1994@gmail.com>
2018-05-17 10:00:30 +0800

    测试：根据用户名查找相应角色/根据角色ID查找权限

--- src/test/java/com/example/mapper/RoleMapperTest.java
+++ src/test/java/com/example/mapper/RoleMapperTest.java
@@ -1,9 +1,15 @@
 package com.example.mapper;
 
+import com.example.manager.entity.Role;
+import com.example.manager.entity.RoleWithPermission;
+import com.example.manager.mapper.RoleMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @author jack
@@ -13,8 +19,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RoleMapperTest {
 
-    @Test
-    public void queryRoleListWithSelected(){
+    @Autowired
+    private RoleMapper mapper;
 
+    @Test
+    public void findUserRole(){
+       List<Role> list = mapper.findUserRole("scott");
+        for (Role role : list) {
+            System.out.println(role.toString());
+        }
     }
+
+    @Test
+    public void roleWithPermission(){
+       List<RoleWithPermission> list = mapper.findById(23L);
+        for (RoleWithPermission roleWithPermission : list) {
+            System.out.println(roleWithPermission.toString());
+        }
+    }
+
 }
