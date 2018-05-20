25a262f4ce689a964ac8d1dea669ff2a95e5fef2
Aslan <jackbu1994@gmail.com>
2018-05-20 20:36:41 +0800

    工具类

--- src/main/java/com/example/util/Constant.java
+++ src/main/java/com/example/util/Constant.java
@@ -6,7 +6,7 @@ package com.example.util;
  * @date 2018/4/25 0025 19:48
  */
 public class Constant {
-    public static final int STATE_LOCKED = 2;
+    public static final String STATE_LOCKED = "0";
 
     /**
      * 每页电影数量
